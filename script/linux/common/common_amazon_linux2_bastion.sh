1524c480a62d61998a18540923eaab962f527d36
maxwell <maxwell@lezhin.com>
2021-04-27 16:35:22 +0900

    update linux userdata script

--- script/linux/common/common_amazon_linux2_bastion.sh
+++ script/linux/common/common_amazon_linux2_bastion.sh
@@ -74,33 +74,9 @@ echo 'alias vi="vim"' >> /etc/bashrc
 echo 'export PS1="\[\e[36;1m\]\u@\[\e[33;1m\]\h:\[\e[37;1m\][\$PWD]\\$ "' >> /etc/bashrc 
 echo 'eval `dircolors /usr/etc/.dircolors`' >> /etc/bashrc
 
-#----------------------------------------------
-# 5. TCP  BottleNeck Bandwidth and RTT
-#----------------------------------------------
-
-modprobe tcp_bbr
-modprobe sch_fq
-sysctl -w net.ipv4.tcp_congestion_control = bbr
-
-cat << EOF >> /etc/sysconfig/modules/tcpcong.modules  
-#!/bin/bash
-
-#--------------------------------------------------------------------------------
-# TCP Bottleneck Bandwidth and RTT
-#               https://aws.amazon.com/ko/amazon-linux-ami/2017.09-release-notes/
-#--------------------------------------------------------------------------------
-
-exec /sbin/modprobe tcp_bbr > /dev/null 2> & 1
-exec /sbin/modprobe sch_fq > /dev/null 2> & 1
-
-EOF
-
-chmod 755 /etc/sysconfig/modules/tcpcong.modules
-echo "net.ipv4.tcp_congestion_control = bbr">> /etc/sysctl.d/00-tcpcong.conf
-
  
 #----------------------------------------------
-# 6. move openfile limit.conf
+# 5. move openfile limit.conf
 #----------------------------------------------
 
 mv ~/package/common/limits.conf /etc/security/limits.conf
